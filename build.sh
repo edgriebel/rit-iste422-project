76c9c432b26c68b5e04790fbbefbe77018eb8542
Ed Griebel <egriebel@marqeta.com>
2020-08-28 21:22:44 -0400

    better error handling on build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-rm *.class
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java || (echo BUILD FAILED!; exit 1)
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest || (echo TESTS FAILED!; exit 1)
+echo "Cleaning existing classes..."
+rm -f *.class
+
+echo "Compiling source code and unit tests..."
+javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
+
+echo "Running unit tests..."
+java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
+
+echo "Running application..."
 java RunEdgeConvert
