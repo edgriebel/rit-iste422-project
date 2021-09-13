89b35366f2b20242239de8a8be06a4529ebc9033
Afzal Ali <71340489+ama3589@users.noreply.github.com>
2021-09-13 03:53:22 +0000

    first commit

--- build.sh
+++ build.sh
@@ -5,13 +5,19 @@ rm -f *.class
 # Note that {} are replaced with each file name
 find . -name \*.class -exec rm {} \;
 
+# echo "Writing the generated class files to a directory called 'build'"
+# javac -d build src/java/*.java
+
+# echo "Writing the generated test class files to a directory called 'build'"
+# javac -d build -cp build test/java/*.java
+
 echo "Compiling source code and unit tests..."
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+javac -d build -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar src/java/*.java test/java/*.java
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:build/EdgeConnectorTest.class org.junit.runner.JUnitCore test/java/EdgeConnectorTest
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java RunEdgeConvert
+java -cp build src/java/RunEdgeConvert
