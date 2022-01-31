d25c3ef9772c6f14ac3fabd4b63f229565e4151b
cclcr21 <cclcr21@rit.edu>
2022-01-31 15:09:18 -0500

    EdgeConnectorTest not found in class path

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ rm -f *.class
 find . -name \*.class -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+javac -cp "./lib/junit-4.12.jar;./lib/hamcrest-core-1.3.jar" ./src/main/java/*.java ./src/test/java/*.java -d build -Xlint:none
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp "./build:.lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:org.junit.runner.JUnitCore" EdgeConnectorTest
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
--- src/test/java/EdgeConnectorTest.java
+++ src/test/java/EdgeConnectorTest.java
@@ -1,7 +1,11 @@
+package src.test.java;
+
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import src.main.java.EdgeConnector;
 
 public class EdgeConnectorTest {
 	EdgeConnector testObj;
