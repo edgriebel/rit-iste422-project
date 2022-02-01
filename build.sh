d5ce0f075c3e152f66b762e3c2fcb8d5e19c901c
cclcr21 <cclcr21@rit.edu>
2022-02-01 16:43:36 -0500

    + mkdir 'build' if need

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ rm -f *.class
 # Note that {} are replaced with each file name
 find . -name \*.class -exec rm {} \;
 
+if [ ! -d "build" ] ; then mkdir build; fi
+
 echo "Compiling source code and unit tests..."
 javac -cp "./lib/junit-4.12.jar;./lib/hamcrest-core-1.3.jar" ./src/main/java/*.java ./src/test/java/*.java -d build -Xlint:none
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
@@ -14,4 +16,4 @@ java -cp "./build:.lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:org.junit.runner
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java RunEdgeConvert
+java -cp "./build" RunEdgeConvert
