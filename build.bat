dfd0709734d471e3cf330c3a2b9f6ab4adaa573d
Replit user <>
2022-09-14 02:42:55 +0000

    requested changes

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-javac -cp lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar *.java
+javac -cp lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar src/main/java/*.java
 java -cp .;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
 java RunEdgeConvert
