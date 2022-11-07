26e0878e1bf836d27c01e71791d0e760521cc016
mlg47 <107060046+MaxGer99@users.noreply.github.com>
2022-11-06 20:24:59 -0500

    Preliminary test added

--- src/test/java/EdgeTableTest.java
+++ src/test/java/EdgeTableTest.java
@@ -5,4 +5,20 @@ import org.junit.Test;
 
 public class EdgeTableTest {
     
+    EdgeTableTest ett;
+
+    @Test
+    public void givenEdgeTableInputStringShouldExist(){
+        assertEquals("There should be an input string", ett.EdgeTable.inputString);
+    }
+
+    @Test
+    public void givenEdgeTableHasCorrectFigureNumber(){
+        assertEquals("Checking the correct figure number", ett.EdgeTable.numFigure);
+    }
+
+    @Test
+    public void givenEdgeTableHasCorrectStringName(){
+        assertEquals("Checking the correct figure number", ett.EdgeTable.name);
+    }
 }
