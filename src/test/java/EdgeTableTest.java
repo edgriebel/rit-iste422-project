b360e38a0f51ee73182dc465ec49dd6a9c63d5ba
mlg47 <107060046+MaxGer99@users.noreply.github.com>
2022-11-08 19:00:50 -0500

    First Test

--- src/test/java/EdgeTableTest.java
+++ src/test/java/EdgeTableTest.java
@@ -5,20 +5,23 @@ import org.junit.Test;
 
 public class EdgeTableTest {
     
-    EdgeTableTest ett;
-
     @Test
-    public void givenEdgeTableInputStringShouldExist(){
-        assertEquals("There should be an input string", ett.EdgeTable.inputString);
+    public void matchInputString(){
+
+        String edg = "11|TestName";
+
+        EdgeTable et = new EdgeTable(edg);        
+        assertEquals("numFigure should match the inputString param", 11, et.getNumFigure());
+        
     }
 
-    @Test
-    public void givenEdgeTableHasCorrectFigureNumber(){
-        assertEquals("Checking the correct figure number", ett.EdgeTable.numFigure);
-    }
+    // @Test
+    // public void givenEdgeTableHasCorrectFigureNumber(){
+    //     assertEquals("Checking the correct figure number", ett.EdgeTable.numFigure);
+    // }
 
-    @Test
-    public void givenEdgeTableHasCorrectStringName(){
-        assertEquals("Checking the correct figure number", ett.EdgeTable.name);
-    }
+    // @Test
+    // public void givenEdgeTableHasCorrectStringName(){
+    //     assertEquals("Checking the correct figure number", ett.EdgeTable.name);
+    // }
 }
