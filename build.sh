22eece251e055887c86d18220bafe6302ca766f8
Ed Griebel <edgriebel@users.noreply.github.com>
2023-09-06 15:39:40 -0400

    remove windows build.bat, expand "-classpath" in build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 echo "Cleaning existing classes..."
 rm -f *.class
 # This command looks for matching files and runs the rm command for each file
@@ -6,7 +7,7 @@ rm -f *.class
 find . -name \*.class -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
@@ -14,4 +15,4 @@ java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCo
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java RunEdgeConvert
+java -classpath . RunEdgeConvert
