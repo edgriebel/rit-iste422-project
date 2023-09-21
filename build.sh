f77c814ae1ffbebb5a2889069e5b91f4be740848
taylor-dennison <tsd2771@rit.edu>
2023-09-21 11:55:56 -0400

    refactor: created directories and adjusted build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+mainDir='src/main/java/'
+testDir='src/test/java/'
 
 echo "Cleaning existing classes..."
 rm -f *.class
@@ -7,12 +9,14 @@ rm -f *.class
 find . -name \*.class -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+javac -d build -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar ${mainDir}*.java ${testDir}*.java 
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
+
+
 echo "Running unit tests..."
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp ./build:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java -classpath . RunEdgeConvert
+java -classpath ./build RunEdgeConvert
