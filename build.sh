224a336e564264892f348dcaab402fdc2d5c73eb
tfudge1 <ndh8546@rit.edu>
2023-09-22 15:23:47 -0400

    dynamically create build dir

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ rm -f *.class
 # Note that {} are replaced with each file name
 find . -name \*.class -exec rm {} \;
 
+echo "making build dir..."
+mkdir build
+
 echo "Compiling source code and unit tests..."
 javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar -d build src/main/java/*.java src/test/java/*.java
 
