ba87570b3afc624b60f32e6ff25504f7b789b124
Gerrit-Wissink <gjw4042@g.rit.edu>
2023-09-26 09:17:10 -0400

    moved java files and modified build.sh to generate class files in a new directory

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ rm -f *.class
 find . -name \*.class -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar ./src/main/java/*.java ./src/test/java/*.java -d build
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:./build org.junit.runner.JUnitCore EdgeConnectorTest
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java -classpath . RunEdgeConvert
+java -classpath .:./build RunEdgeConvert
