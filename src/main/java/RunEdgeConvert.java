d848eb2cd31cbcd5ec2afe69b4f7561dd176c790
tfudge1 <ndh8546@rit.edu>
2023-10-18 16:30:30 -0400

    added logging

--- src/main/java/RunEdgeConvert.java
+++ src/main/java/RunEdgeConvert.java
@@ -1,7 +1,11 @@
-// package src.main.java;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class RunEdgeConvert {
+   private static Logger logger = LogManager.getLogger(RunEdgeConvert.class.getName());
+
    public static void main(String[] args) {
+      logger.info("Starting application {}", RunEdgeConvert.class);
       EdgeConvertGUI edge = new EdgeConvertGUI();
    }
 }
