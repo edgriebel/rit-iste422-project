0d9797d5a47da8b5d610fc05e9a5cec21b566b01
mep4741 <mep4741@rit.edu>
2024-01-31 17:03:15 -0500

    partially thru, changes made but build doesnt work

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ echo "Cleaning existing classes..."
 rm -f *.class
 # This command looks for matching files and runs the rm command for each file
 # Note that {} are replaced with each file name
-find . -name \*.class -exec rm {} \;
+find . -name ./build -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar ./src/main/java -d ./build
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
@@ -15,4 +15,4 @@ java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCo
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java -classpath . RunEdgeConvert
+java -classpath ./build RunEdgeConvert
---
+++ src/main/java/RunEdgeConvert.java
@@ -0,0 +1,6 @@
+package src.main.java;
+public class RunEdgeConvert {
+   public static void main(String[] args) {
+      EdgeConvertGUI edge = new EdgeConvertGUI();
+   }
+}
