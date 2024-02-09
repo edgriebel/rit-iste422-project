2a6910adc4fc9106b0bcfedd1f53a75d714ce28e
mep4741 <mep4741@rit.edu>
2024-02-09 08:47:03 -0500

    fixed build classpath line

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ rm -f *.class
 find . -name ./build -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar ./src/main/java -d ./build
+javac -classpath .:src/main/java/*.java .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar -d ./build
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
